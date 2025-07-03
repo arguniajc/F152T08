a0f8acb0c04e7b1199c97a069afe4a00d4bcd55a
Diego Fernando Salazar Rosero <dfsalazarr@siesa.com>
2025-07-03 16:46:31 -0500

    OK 8

--- PRACTICA/JS/Ejer08.js
+++ PRACTICA/JS/Ejer08.js
@@ -1,1 +1,13 @@
 /* Problema 8: Analizador de Texto */
+let texto = " Aprender JavaScript es muy Divertido ";
+let Textoimpio =  texto.trim();
+
+console.log("Texto original: " + texto);
+console.log("Longitud original: " + texto.length);
+console.log("Texto limpio: " + Textoimpio);
+console.log("Longitud limpia: " + Textoimpio.length);
+console.log("En mayúsculas: " + texto.toUpperCase());
+console.log("En minúsculas: " + texto.toLowerCase());
+console.log("Primer carácter: " + texto.trim().charAt(0));
+console.log("Último carácter: " + texto.trim().charAt(texto.trim().length - 1));
+ 
