038d3afd0ae9ac96bfd39d7309b24c9b8c4faf83
Diego Fernando Salazar Rosero <dfsalazarr@siesa.com>
2025-07-03 15:19:59 -0500

    ok

--- PRACTICA/JS/Ejer03.js
+++ PRACTICA/JS/Ejer03.js
@@ -1,1 +1,7 @@
 /* Problema 3: Generador de Números de Lotería */
+let numero1 = Math.floor(Math.random() * 50) + 1;
+let numero2 = Math.floor(Math.random() * 50) + 1;
+let numero3 = Math.floor(Math.random() * 50) + 1;
+let numero4 = Math.floor(Math.random() * 50) + 1;
+let numero5 = Math.floor(Math.random() * 50) + 1;
+console.log(`Numeros de loteria: ${numero1}, ${numero2}, ${numero3}, ${numero4}, ${numero5}`);
