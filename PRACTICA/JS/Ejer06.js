4ef739cb4915baf498a884f69d73f10e4d722a85
Diego Fernando Salazar Rosero <dfsalazarr@siesa.com>
2025-07-03 15:46:26 -0500

    ok

--- PRACTICA/JS/Ejer06.js
+++ PRACTICA/JS/Ejer06.js
@@ -1,1 +1,7 @@
 /* Problema 6: Área y Perímetro de un Círculo */
+let radio = 7.5;
+let area = Math.PI * Math.pow(radio, 2);
+let perimetro = 2 * Math.PI * radio;
+console.log(`Radio: ${radio} metros`);
+console.log(`Area: ${area.toFixed(2)} metros cuadrados`);
+console.log(`Perimetro: ${perimetro.toFixed(2)} metros`);
