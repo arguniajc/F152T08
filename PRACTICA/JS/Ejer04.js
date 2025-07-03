80da546f448f4d0d1f16bd20382f033004226a47
Diego Fernando Salazar Rosero <dfsalazarr@siesa.com>
2025-07-03 15:29:42 -0500

    ok

--- PRACTICA/JS/Ejer04.js
+++ PRACTICA/JS/Ejer04.js
@@ -1,1 +1,13 @@
 /* Problema 4: Calculadora de Interés Simple */
+/* Monto Final = Capital + (Capital × Tasa × Tiempo) */
+let capital = 5000;
+let tasa = 3.5 / 100; 
+let tiempo = 2;
+let Interes = capital*tasa*tiempo;
+let MontoFinal = capital + Interes;
+
+console.log(`Capacidad inicial: $${capital}`);
+console.log(`Capacidad interés: ${(tasa * 100).toFixed(1)}% anual`);
+console.log(`Tiempo: ${tiempo} años`);
+console.log(`Interes ganado: $${Interes.toFixed(0)}`);
+console.log(`Monto final: $${MontoFinal}`);
